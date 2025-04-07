63ecbba8ddef8023f5c144a6fa2c237a8e8c1cf7
otie16 <biggieestone@gmail.com>
2025-04-07 23:16:14 +0100

    created route table resources

--- main.tf
+++ main.tf
@@ -1,30 +1,69 @@
 provider "aws" {
-  region =  "us-east-1"
+  region = "us-east-1"
 }
 
-variable "cidr_blocks" {
-  description = "cidr blocks & name tags for vpc and subnets"
-#   type = list(string)
-type = list(object({
-  cidr_block = string
-  name = string 
-}))
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+   
 
-
-resource "aws_vpc" "dev_vpc" {
+# Creating a VPC
+resource "aws_vpc" "myapp_vpc" {
 #   cidr_block = var.cidr_blocks[0]
-cidr_block = var.cidr_blocks[0].cidr_block
+cidr_block = var.vpc_cidr_block
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev_subnet_1" {
-    vpc_id = aws_vpc.dev_vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
-    availability_zone = "us-east-1a"
+# Creating a subnet
+resource "aws_subnet" "myapp_subnet_1" {
+    vpc_id = aws_vpc.myapp_vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-      "Name": "subnet-1-dev" 
+      "Name": "${var.env_prefix}-subnet-1" 
     }
 }
+
+# Creating internet gateway
+resource "aws_internet_gateway" "myapp-gateway" {
+  vpc_id = aws_vpc.myapp_vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+# configure the default route table
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-gateway.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+} 
+
+# Creating route table for vpc
+# resource "aws_route_table" "myapp-route-table" {
+#   vpc_id = aws_vpc.myapp_vpc.id
+
+#   route {
+#       cidr_block = "0.0.0.0/0"
+#       gateway_id = aws_internet_gateway.myapp-gateway.id
+#   }
+
+#   tags = {
+#     Name: "${var.env_prefix}-rtb"
+#   }
+# }
+
+# # setup route table and subnet association
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp_subnet_1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
